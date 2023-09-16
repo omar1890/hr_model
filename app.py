82ef76530335e12c7907a3fb2826321db6a15628
Omar Wael <omar_wael1890@yahoo.com>
2023-09-16 22:31:57 +0300

    app changed

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from docx import Document
 import spacy
 from spacy.matcher import PhraseMatcher
 from sklearn.metrics.pairwise import cosine_similarity  # Import cosine_similarity
+from io import BytesIO
+import tempfile
 
 # load default skills data base
 from skillNer.general_params import SKILL_DB
@@ -19,37 +21,40 @@ nlp = spacy.load("en_core_web_md")
 skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher) 
 
 def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
+        with open(pdf_path, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            text = ''
+            for page_num in range(len(reader.pages)):
+                text += reader.pages[page_num].extract_text()
+        return text
+
+def extract_text_from_docx(docx_file):
+    try:
+        doc = Document(docx_file)
         text = ''
-        for page_num in range(len(reader.pages)):
-            text += reader.pages[page_num].extract_text()
-    return text
+        for para in doc.paragraphs:
+            text += para.text + '\n'
+        return text
+    except Exception as e:
+        return str(e)
 
-def extract_text_from_docx(docx_path):
-    doc = Document(docx_path)
-    text = ''
-    for para in doc.paragraphs:
-        text += para.text + '\n'
-    return text
-
-
-def extract_text_from_folder(folder_path):
-    extracted_text_dict = {}  # Initialize an empty dictionary
-    
-    for root, _, files in os.walk(folder_path):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            if file_name.lower().endswith('.pdf'):
-                extracted_text = extract_text_from_pdf(file_path)
-            elif file_name.lower().endswith('.docx'):
-                extracted_text = extract_text_from_docx(file_path)
+def extract_text_from_folder(files):
+        extracted_text_dict = {}  # Initialize an empty dictionary
+        temp_dir = tempfile.TemporaryDirectory(prefix='pdf_temp')
+        for file in files:
+            filename = file.filename
+            if filename.endswith('.pdf'):
+                pdf_path = os.path.join(temp_dir.name, filename)
+                file.save(pdf_path)
+                extracted_text = extract_text_from_pdf(pdf_path)
+            elif filename.endswith('.docx'):
+                extracted_text = extract_text_from_docx(file)
             else:
                 extracted_text = ''  # Handle other file types if needed
             
-            extracted_text_dict[file_name] = extracted_text  # Store text with filename as key
-    
-    return extracted_text_dict
+            extracted_text_dict[filename] = extracted_text  # Store text with filename as key
+        
+        return extracted_text_dict
 
 # Function to calculate skill match score (cosine similarity)
 def calculate_skill_match_score(resume_text, job_description_text):
@@ -61,26 +66,19 @@ def calculate_skill_match_score(resume_text, job_description_text):
     similarity = resume_doc.similarity(job_description_doc)
 
     return similarity
-
-@app.route('/', methods=['GET'])
-def test():
-    return "test"
-
+	
 @app.route('/extract-text', methods=['POST'])
 def extract_text():
-    if 'folder' not in request.form:
-        return jsonify({'error': 'No folder path provided'})
-
-    folder_path = request.form['folder']
-    if not os.path.exists(folder_path):
-        return jsonify({'error': 'Folder not found'})
+    files = request.files.getlist('files')  # 'files' should be the name of the input field in your HTML form
+    if not files:
+        return jsonify({'error': 'No files provided'})
 
     if 'job_description' not in request.form:
         return jsonify({'error': 'No job description provided'})
 
     job_description = request.form['job_description']
 
-    extracted_text_dict = extract_text_from_folder(folder_path)
+    extracted_text_dict = extract_text_from_folder(files)
 
     # Convert job description and its skills to a single text
     job_description_annotations = skill_extractor.annotate(job_description)
@@ -118,6 +116,10 @@ def extract_text():
 
     return jsonify(result_dict)
 
+
+@app.route('/', methods=['GET'])
+def test():
+    return "test"
 
 if __name__ == '__main__':
     app.run()
